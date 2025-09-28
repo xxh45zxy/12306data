1600751663d0f108688fd51ed4bf1311f8acd8cf
xxh45zxy <xxh45zxy@126.com>
2025-09-28 17:02:19 +0800

    add commute ticket function

---
+++ script/railwaycomminvoice.py
@@ -0,0 +1,102 @@
+import json  
+import pandas as pd
+from general_func import *
+
+
+def railwaycomminvoicedata(file_name,check_id,sort_id,input_type,*input_json_data):
+
+    existing_data = read_existing_data(file_name)
+
+    # 不断从用户那里获取JSON输入，直到用户直接回车  
+    all_new_data = pd.DataFrame()  # 用于存储所有新输入的数据
+
+    count_data = 0
+    
+    while True:
+
+        count_data += 1
+        if count_data>1 and input_type == 0:
+            break
+
+        if input_type == 1:
+            
+            input_json = manual_input()
+            
+            if not input_json.strip():  # 如果用户输入的是空字符串或只有空格  
+                break  
+ 
+        try:  
+            # 尝试解析JSON数据
+            if input_type == 1:
+                data_s = json.loads(input_json)
+            else:
+                data_s = input_json_data[0]
+              
+            # 初始化一个空列表来存储转换后的数据行  
+            rows = []
+            # print("o1")
+              
+            # 遍历orderList中的每个项目
+            for data in data_s:
+
+                for order in data['data']['results']:
+                    
+                    order_new = {}
+                    
+                    for key, value in order.items():
+
+                        if value is None:
+                            order_new[key] = ""
+                        elif isinstance(value, list):
+                            if value == []:
+                                order_new[f"{key}_1"] = ""
+
+                            elif key == "batchList":
+                                value_seq = -1
+                                for value_s in value:
+                                    value_seq += 1
+                                    for key_t, value_t in value_s.items():
+                                        key_ts = "batchList_" + str(value_seq) + "_" + key_t
+                                        if value_t is None:
+                                            order_new[key_ts] = ""
+                                        else:
+                                            order_new[key_ts] = str(value_t)
+
+                            # 如果值是列表，则拆分列表并添加新的键值对
+                            else:
+                                for i, item in enumerate(value, start=1):
+                                    if item is None:
+                                        item = ""
+                                    new_key = f"{key}_{i}"  
+                                    order_new[new_key] = str(item)
+
+                                if key == "orderinfo" and len(value) == 2:
+                                    order_new["orderinfo_3"] = order_new["orderinfo_2"]
+                                    order_new["orderinfo_2"] = ""
+                        else:  
+                            # 如果值不是列表，则直接添加原始的键值对  
+                            order_new[key] = str(value)
+                                    
+                    rows.append(order_new)
+
+            # print("o2")  
+            # 将行列表转换为DataFrame  
+            df_new = pd.DataFrame(rows)  
+            all_new_data = pd.concat([all_new_data, df_new], ignore_index=True)
+            # print("o3")
+      
+        except json.JSONDecodeError:  
+            print("输入的JSON数据格式错误，请重新输入。")  
+
+    save_csv(file_name,check_id,sort_id,existing_data,all_new_data,1,'last')
+
+
+if __name__ == '__main__':
+    
+    file_name_1 = "railwaycomminvoice.csv"
+    check_id_1 = "orderId"
+    sort_id_1 = "saleTime"
+
+    railwaycomminvoicedata(file_name_1,check_id_1,sort_id_1,1)
+
+    print("已完成。")
---
+++ script/railwaycommorder.js
@@ -0,0 +1,49 @@
+// 初始化一个空对象来存储所有返回的 JSON 数据
+const allData = {};
+
+// 创建一个全局变量来存储结果
+window.fetchResult = null;
+
+// 定义一个函数来接收传入的 JSON 字符串
+function init(jsonString) {
+    try {
+        // 将 JSON 字符串解析为 JavaScript 对象或数组
+        const trade_info_list = jsonString[0];
+        // 创建一个数组来存储所有的 Promise
+        const promises = trade_info_list.map((info, index) => {
+            const query_type = info;
+            
+            // 执行 fetch 请求并返回一个 Promise
+            return fetch('https://kyfw.12306.cn/otn/npQuery/queryFavorOrderMergeInfoNew', {
+                method: 'POST',
+                headers: {
+                    "content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+                },
+                body: `query_type=${encodeURIComponent(query_type)}&page_index=1&page_size=10`
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+                return response.json();
+            })
+            .then(data => {
+                // 将返回的 JSON 数据存储到 allData 对象中
+                allData[index] = data;
+                return data; // 确保 Promise 解析为 data
+            })
+            .catch(error => {
+                console.error('There has been a problem with your fetch operation:', error);
+                return null; // 在错误情况下返回一个 null，避免 Promise 被拒绝
+            });
+        });
+
+        // 使用 Promise.all 等待所有请求完成
+        Promise.all(promises)
+            .then(() => {
+                window.fetchResult = JSON.stringify(allData, null, 2);
+            });
+    } catch (error) {
+        console.error("Error parsing JSON:", error);
+    }
+}
